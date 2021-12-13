f9423d4f58a7e5f572f579d2886d14eb31dfbd54
埃博拉酱 <vhtmfu@outlook.com>
2021-12-13 19:21:06 +0800

    1.1.0新增功能 新增支持写入不前缀长度的字符串 新增支持对操作记录进行载入、保存、复制操作 新增退出应用时保存状态，下次启动时还原

--- 二进制文件读写器/MainPage.xaml.vb
+++ 二进制文件读写器/MainPage.xaml.vb
@@ -2,61 +2,81 @@
 Imports Windows.Storage
 Imports Windows.Storage.Streams
 Imports System.Text
+Imports Windows.Storage.Pickers
+
 
 ''' <summary>
 ''' An empty page that can be used on its own or navigated to within a Frame.
 ''' </summary>
 Public NotInheritable Class MainPage
 	Inherits Page
-	Private 文件打开对话框 As New Pickers.FileOpenPicker
-	Private 文件新建对话框 As New Pickers.FileSavePicker
+	Private 文件打开对话框 As New FileOpenPicker
+	Private 文件新建对话框 As New FileSavePicker
+	Private 保存记录对话框 As New FileSavePicker
+	Private 载入记录对话框 As New FileOpenPicker
 	Private 当前流 As Stream
 	Private 消息列表 As New ObservableCollection(Of String)
-	Sub New()
-
-		' This call is required by the designer.
-		InitializeComponent()
-
-		' Add any initialization after the InitializeComponent() call.
-		文件打开对话框.FileTypeFilter.Add("*")
-		文件新建对话框.FileTypeChoices.Add("用户定义类型", New List(Of String) From {".userdefined"})
-		操作记录.ItemsSource = 消息列表
-	End Sub
-	Private Function 参数检查() As Long
+	Shared ReadOnly 漫游设置 As ApplicationDataContainer = ApplicationData.Current.RoamingSettings
+	Shared ReadOnly 漫游目录 As StorageFolder = ApplicationData.Current.RoamingFolder
+	Const 操作记录路径 As String = "操作记录.操作记录"
+	ReadOnly 操作记录获取任务 As IAsyncOperation(Of IStorageItem) = 漫游目录.TryGetItemAsync(操作记录路径)
+	Shared ReadOnly 最近访问列表 As AccessCache.StorageItemMostRecentlyUsedList = AccessCache.StorageApplicationPermissions.MostRecentlyUsedList
+	Private 操作记录流 As Stream
+	Private 操作记录读取器 As BinaryReader
+	Private 操作记录写出器 As BinaryWriter
+	Private 当前流读取器 As BinaryReader
+	Private 当前流写出器 As BinaryWriter
+	Private Function 参数检查(错误提示位置 As FrameworkElement) As Long
 		If 当前流 Is Nothing Then
 			错误内容.Text = "未打开任何文件"
-			错误提示.ShowAt(打开文件)
+			错误提示.ShowAt(错误提示位置)
 			Return -1
 		Else
 			Try
 				当前流.Position = Long.Parse(当前位置.Text)
 			Catch ex As Exception
 				错误内容.Text = ex.Message
-				错误提示.ShowAt(当前位置)
+				错误提示.ShowAt(错误提示位置)
 				Return -1
 			End Try
 			Try
 				Return Long.Parse(读写次数.Text)
 			Catch ex As Exception
 				错误内容.Text = ex.Message
-				错误提示.ShowAt(读写次数)
+				错误提示.ShowAt(错误提示位置)
 				Return -1
 			End Try
 		End If
 	End Function
-	Private Async Sub 设置当前文件(文件操作 As IAsyncOperation(Of StorageFile))
-		Dim 文件 As StorageFile = Await 文件操作
+	Private Async Sub 设置当前文件(文件操作 As IAsyncOperation(Of StorageFile), Optional 加入最近列表 As Boolean = True)
+		Dim 文件 As StorageFile
+		Try
+			文件 = Await 文件操作
+		Catch ex As FileNotFoundException
+			Exit Sub
+		End Try
 		If 文件 IsNot Nothing Then
 			If 当前流 IsNot Nothing Then
 				当前流.Close()
 			End If
 			当前流 = (Await 文件.OpenAsync(FileAccessMode.ReadWrite)).AsStream
+			当前流读取器 = New BinaryReader(当前流)
+			当前流写出器 = New BinaryWriter(当前流)
 			文件路径.Text = 文件.Path
-			当前位置.Text = 当前流.Position
+			If 加入最近列表 Then
+				漫游设置.Values("上次打开文件令牌") = 最近访问列表.Add(文件)
+				当前位置.Text = 当前流.Position
+			Else
+				当前流.Position = 漫游设置.Values("当前位置")
+			End If
 		End If
 	End Sub
 	Private Sub 打开文件_Click(sender As Object, e As RoutedEventArgs) Handles 打开文件.Click
 		设置当前文件(文件打开对话框.PickSingleFileAsync)
+	End Sub
+	Private Sub 添加消息(消息 As String)
+		消息列表.Add(消息)
+		操作记录写出器.Write(消息)
 	End Sub
 	Private Sub 常规读入(Of T)(提示词 As String, 单读函数 As Func(Of T), 操作次数 As UInteger)
 		Dim 字符串 As New StringBuilder("从位置")
@@ -65,62 +85,62 @@ Public NotInheritable Class MainPage
 			Try
 				字符串.Append(单读函数.Invoke).Append(" ")
 			Catch ex As Exception
-				消息列表.Add(字符串.ToString)
-				消息列表.Add("出错：" & ex.Message)
+				添加消息(字符串.ToString)
+				添加消息("出错：" & ex.Message)
 				Throw ex
 			End Try
 		Next
-		消息列表.Add(字符串.ToString)
+		Dim 结果 As String = 字符串.ToString
+		添加消息(结果)
 	End Sub
 	Private Sub 二进制读入_Click(sender As Object, e As RoutedEventArgs) Handles 二进制读入.Click
-		Dim 操作次数 As Long = 参数检查()
+		Dim 操作次数 As Long = 参数检查(sender)
 		Static 操作列表 As Action(Of UInteger)() = {
 	Sub(次数 As UInteger)
-		常规读入("读入布尔逻辑：", AddressOf New BinaryReader(当前流).ReadBoolean, 次数)
+		常规读入("读入布尔逻辑：", AddressOf 当前流读取器.ReadBoolean, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入8位无符号：", AddressOf New BinaryReader(当前流).ReadByte, 次数)
+		常规读入("读入8位无符号：", AddressOf 当前流读取器.ReadByte, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入8位有符号：", AddressOf New BinaryReader(当前流).ReadSByte, 次数)
+		常规读入("读入8位有符号：", AddressOf 当前流读取器.ReadSByte, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入16位无符号：", AddressOf New BinaryReader(当前流).ReadUInt16, 次数)
+		常规读入("读入16位无符号：", AddressOf 当前流读取器.ReadUInt16, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入16位有符号：", AddressOf New BinaryReader(当前流).ReadInt16, 次数)
+		常规读入("读入16位有符号：", AddressOf 当前流读取器.ReadInt16, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入32位无符号：", AddressOf New BinaryReader(当前流).ReadUInt32, 次数)
+		常规读入("读入32位无符号：", AddressOf 当前流读取器.ReadUInt32, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入32位有符号：", AddressOf New BinaryReader(当前流).ReadInt32, 次数)
+		常规读入("读入32位有符号：", AddressOf 当前流读取器.ReadInt32, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入64位无符号：", AddressOf New BinaryReader(当前流).ReadUInt64, 次数)
+		常规读入("读入64位无符号：", AddressOf 当前流读取器.ReadUInt64, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入64位有符号：", AddressOf New BinaryReader(当前流).ReadInt64, 次数)
+		常规读入("读入64位有符号：", AddressOf 当前流读取器.ReadInt64, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入十进制：", AddressOf New BinaryReader(当前流).ReadDecimal, 次数)
+		常规读入("读入十进制：", AddressOf 当前流读取器.ReadDecimal, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入单精度：", AddressOf New BinaryReader(当前流).ReadSingle, 次数)
+		常规读入("读入单精度：", AddressOf 当前流读取器.ReadSingle, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
-		常规读入("读入双精度：", AddressOf New BinaryReader(当前流).ReadDouble, 次数)
+		常规读入("读入双精度：", AddressOf 当前流读取器.ReadDouble, 次数)
 	End Sub,
 	Sub(次数 As UInteger)
 		Dim 当前位置 As ULong = 当前流.Position
-		Dim 字符 As Char() = New BinaryReader(当前流).ReadChars(次数)
-		消息列表.Add("从位置" & 当前位置 & "读入字符：" & 字符)
+		Dim 字符 As Char() = 当前流读取器.ReadChars(次数)
+		添加消息("从位置" & 当前位置 & "读入字符：" & 字符)
 	End Sub,
 	Sub(次数 As UInteger)
-		Dim 读取器 As New BinaryReader(当前流)
-		消息列表.Add("从位置" & 当前流.Position & "读入字符串：")
+		添加消息("从位置" & 当前流.Position & "读入字符串：")
 		For a As UInteger = 1 To 次数
-			消息列表.Add("	" + 读取器.ReadString)
+			添加消息("	" + 当前流读取器.ReadString)
 		Next
 	End Sub}
 		If 操作次数 > 0 Then
@@ -130,7 +150,13 @@ Public NotInheritable Class MainPage
 				错误内容.Text = ex.Message
 				错误提示.ShowAt(二进制读入)
 			End Try
-			当前位置.Text = 当前流.Position
+			If 自动递进位置.IsChecked Then
+				当前位置.Text = 当前流.Position
+			Else
+				当前流.Position = 当前位置.Text
+			End If
+			漫游设置.Values("当前位置") = 当前流.Position
+			漫游设置.Values("读写次数") = 操作次数
 		End If
 	End Sub
 
@@ -153,16 +179,16 @@ Public NotInheritable Class MainPage
 		Try
 			System.Buffer.BlockCopy(Enumerable.Repeat(Parsed内容, 操作次数).ToArray, 0, 字节, 0, 字节数)
 		Catch ex As Exception
-			消息列表.Add(提示词 & "（出错）")
-			消息列表.Add("出错：" & ex.Message)
+			添加消息(提示词 & "（出错）")
+			添加消息("出错：" & ex.Message)
 			Throw ex
 		End Try
 		当前流.WriteAsync(字节, 0, 字节数)
 		当前流.FlushAsync()
-		消息列表.Add(提示词)
+		添加消息(提示词)
 	End Sub
 	Private Sub 二进制写出_Click(sender As Object, e As RoutedEventArgs) Handles 二进制写出.Click
-		Dim 操作次数 As Long = 参数检查()
+		Dim 操作次数 As Long = 参数检查(sender)
 		Static 操作列表 As Action(Of UInteger)() = {
 	Sub(次数 As UInteger) 常规写出("写出布尔逻辑：", AddressOf Boolean.Parse, 次数, 1),
 	Sub(次数 As UInteger)
@@ -171,12 +197,12 @@ Public NotInheritable Class MainPage
 		Try
 			当前流.WriteAsync(Enumerable.Repeat(Byte.Parse(内容), 次数).ToArray, 0, 次数)
 		Catch ex As Exception
-			消息列表.Add(提示词 & "（出错）")
-			消息列表.Add("出错：" & ex.Message)
+			添加消息(提示词 & "（出错）")
+			添加消息("出错：" & ex.Message)
 			Throw ex
 		End Try
 		当前流.FlushAsync()
-		消息列表.Add(提示词)
+		添加消息(提示词)
 	End Sub,
 	Sub(次数 As UInteger) 常规写出("写出8位有符号：", AddressOf SByte.Parse, 次数, 1),
 	Sub(次数 As UInteger) 常规写出("写出16位无符号：", AddressOf UShort.Parse, 次数, 2),
@@ -186,37 +212,34 @@ Public NotInheritable Class MainPage
 	Sub(次数 As UInteger) 常规写出("写出64位无符号：", AddressOf ULong.Parse, 次数, 8),
 	Sub(次数 As UInteger) 常规写出("写出64位有符号：", AddressOf Long.Parse, 次数, 8),
 	Sub(次数 As UInteger)
-		Dim 写出器 As New BinaryWriter(当前流)
 		Dim 内容 As String = 写出内容.Text
 		Dim Parsed内容 As Decimal = Decimal.Parse(内容)
 		For a As UInteger = 1 To 次数
-			写出器.Write(Parsed内容)
+			当前流写出器.Write(Parsed内容)
 		Next
-		消息列表.Add("写出十进制：" & 内容 & " ×" & 次数)
+		添加消息("写出十进制：" & 内容 & " ×" & 次数)
 	End Sub,
 	Sub(次数 As UInteger) 常规写出("写出单精度：", AddressOf Single.Parse, 次数, 4),
 	Sub(次数 As UInteger) 常规写出("写出双精度：", AddressOf Double.Parse, 次数, 8),
 	Sub(次数 As UInteger)
 		Dim 内容 As String = 写出内容.Text
-		Dim 写出器 As New BinaryWriter(当前流)
 		Dim 提示词 As String = "向位置" & 当前流.Position & "写出字符：" & 内容 & "×" & 次数
 		Try
-			写出器.Write(Enumerable.Repeat(Char.Parse(内容), 次数).ToArray)
+			当前流写出器.Write((New StringBuilder).AppendJoin("", Enumerable.Repeat(内容, 次数)).ToString.ToCharArray)
 		Catch ex As Exception
-			消息列表.Add(提示词 & "（出错）")
-			消息列表.Add("出错：" & ex.Message)
+			添加消息(提示词 & "（出错）")
+			添加消息("出错：" & ex.Message)
 			Throw ex
 		End Try
-		消息列表.Add(提示词)
+		添加消息(提示词)
 	End Sub,
 	Sub(次数 As UInteger)
-		Dim 写出器 As New BinaryWriter(当前流)
 		Dim 内容 As String = 写出内容.Text
 		Dim 提示词 As String = "向位置" & 当前流.Position & "写出字符串（前缀长度）：" & 内容 & "×" & 次数
 		For a As UInteger = 1 To 次数
-			写出器.Write(内容)
+			当前流写出器.Write(内容)
 		Next
-		消息列表.Add(提示词)
+		添加消息(提示词)
 	End Sub}
 		If 操作次数 > 0 Then
 			Try
@@ -225,11 +248,18 @@ Public NotInheritable Class MainPage
 				错误内容.Text = ex.Message
 				错误提示.ShowAt(二进制读入)
 			End Try
-			当前位置.Text = 当前流.Position
+			If 自动递进位置.IsChecked Then
+				当前位置.Text = 当前流.Position
+			Else
+				当前流.Position = 当前位置.Text
+			End If
+			漫游设置.Values("当前位置") = 当前流.Position
+			漫游设置.Values("读写次数") = 操作次数
+			漫游设置.Values("写出内容") = 写出内容.Text
 		End If
 	End Sub
 
-	Private Sub 关闭文件_Click(sender As Object, e As RoutedEventArgs) Handles 关闭文件.Click
+	Private Sub 关闭文件_Click() Handles 关闭文件.Click
 		If 当前流 Is Nothing Then
 			错误内容.Text = "未打开任何文件"
 			错误提示.ShowAt(打开文件)
@@ -243,5 +273,99 @@ Public NotInheritable Class MainPage
 
 	Private Sub 清除记录_Click(sender As Object, e As RoutedEventArgs) Handles 清除记录.Click
 		消息列表.Clear()
+		操作记录写出器.Seek(0, SeekOrigin.Begin)
+		操作记录写出器.Write(CUShort(0))
+	End Sub
+
+	Private Sub 自动递进位置_Checked(sender As Object, e As RoutedEventArgs) Handles 自动递进位置.Checked, 自动递进位置.Unchecked
+		漫游设置.Values("自动递进位置") = 自动递进位置.IsChecked
+	End Sub
+
+	Private Sub 关闭处理()
+		Dim 流位置 As UInteger = 操作记录流.Position
+		操作记录写出器.Seek(0, SeekOrigin.Begin)
+		操作记录写出器.Write(CUShort(消息列表.Count))
+		操作记录写出器.Seek(流位置, SeekOrigin.Begin)
+		漫游设置.Values("数据类型") = 数据类型.SelectedIndex
+	End Sub
+
+	Private Async Sub 保存记录_Click(sender As Object, e As RoutedEventArgs) Handles 保存记录.Click
+		Dim 记录文件 As StorageFile = Await 保存记录对话框.PickSaveFileAsync()
+		If 记录文件 IsNot Nothing Then
+			Dim 记录写出器 As New BinaryWriter(Await 记录文件.OpenStreamForWriteAsync)
+			记录写出器.Write(CUShort(消息列表.Count))
+			For L As Short = 0 To 消息列表.Count - 1
+				记录写出器.Write(消息列表(L))
+			Next
+			记录写出器.Close()
+		End If
+	End Sub
+	Private Async Sub 载入记录(覆盖 As Boolean)
+		Dim 记录文件 As StorageFile = Await 载入记录对话框.PickSingleFileAsync
+		If 记录文件 IsNot Nothing Then
+			Dim 记录读入器 As New BinaryReader(Await 记录文件.OpenStreamForWriteAsync)
+			If 覆盖 Then
+				消息列表.Clear()
+				操作记录写出器.Seek(2, SeekOrigin.Begin)
+			End If
+			For L As UShort = 1 To 记录读入器.ReadUInt16
+				添加消息(记录读入器.ReadString)
+			Next
+			记录读入器.Close()
+		End If
+	End Sub
+	Private Sub 追加记录_Click(sender As Object, e As RoutedEventArgs) Handles 追加记录.Click
+		载入记录(False)
+	End Sub
+
+	Private Sub 覆盖记录_Click(sender As Object, e As RoutedEventArgs) Handles 覆盖记录.Click
+		载入记录(True)
+	End Sub
+
+	Private Async Sub MainPage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+		文件打开对话框.FileTypeFilter.Add("*")
+		载入记录对话框.FileTypeFilter.Add(".操作记录")
+		文件新建对话框.FileTypeChoices.Add("用户定义类型", New List(Of String) From {".userdefined"})
+		保存记录对话框.FileTypeChoices.Add("操作记录文件", New List(Of String) From {".操作记录"})
+		操作记录.ItemsSource = 消息列表
+		自动递进位置.IsChecked = If(漫游设置.Values("自动递进位置"), True)
+		数据类型.SelectedIndex = If(漫游设置.Values("数据类型"), 0)
+		读写次数.Text = If(漫游设置.Values("读写次数"), "")
+		写出内容.Text = If(漫游设置.Values("写出内容"), "")
+		Dim 操作记录文件 As StorageFile = Await 操作记录获取任务
+		If 操作记录文件 Is Nothing Then
+			操作记录文件 = Await 漫游目录.CreateFileAsync(操作记录路径)
+		End If
+		操作记录流 = (Await 操作记录文件.OpenAsync(FileAccessMode.ReadWrite)).AsStream
+		操作记录读取器 = New BinaryReader(操作记录流)
+		操作记录写出器 = New BinaryWriter(操作记录流)
+		Dim 记录数 As UShort = 0
+		Try
+			记录数 = 操作记录读取器.ReadUInt16
+		Catch ex As EndOfStreamException
+			操作记录写出器.Write(CUShort(0))
+		End Try
+		Dim L As UShort
+		Try
+			For L = 1 To 记录数
+				'此处不能用添加消息，自己添加给自己？
+				消息列表.Add(操作记录读取器.ReadString)
+			Next
+		Catch ex As FormatException
+			Dim 当前流位置 As UInteger = 操作记录流.Position
+			操作记录流.Seek(0, SeekOrigin.Begin)
+			操作记录写出器.Write(L)
+			操作记录流.Seek(当前流位置, SeekOrigin.Begin)
+		End Try
+		Dim 上次打开文件令牌 As String = 漫游设置.Values("上次打开文件令牌")
+		If 上次打开文件令牌 IsNot Nothing Then
+			设置当前文件(最近访问列表.GetFileAsync(上次打开文件令牌), False)
+		End If
+		扩展名.Text = If(漫游设置.Values("扩展名"), ".bin")
+		当前位置.Text = If(漫游设置.Values("当前位置"), 0)
+		If 当前流 IsNot Nothing Then
+			当前流.Seek(当前位置.Text, SeekOrigin.Begin)
+		End If
+		DirectCast(App.Current, App).注册关闭处理(AddressOf 关闭处理)
 	End Sub
 End Class
